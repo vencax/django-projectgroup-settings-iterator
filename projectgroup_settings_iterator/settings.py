e94b6d6ebb92c98639a3773c4da669179465e424
Vaclav Klecanda <vencax77@gmail.com>
2013-02-01 10:08:53 +0100

    import error catcher minor changes

--- projectgroup_settings_iterator/settings.py
+++ projectgroup_settings_iterator/settings.py
@@ -51,15 +51,18 @@ class Settings(object):
             if not os.path.isdir(projPath) or project.startswith('.'):
                 continue
 
+            settingsFldr = os.path.join(projPath, self.path_to_settings)
             try:
-                proj_sett = self._load_project_module(projPath)
+                proj_sett = self._load_project_module(settingsFldr)
                 self.process_project_settins(proj_sett, projPath)
+            except ImportError:
+                logging.debug('no %s in %s' % \
+                    (self.settings_module_name, settingsFldr))
             except Exception, e:
                 logging.exception(e)
-        logging.debug('Settings loaded')
+        logging.debug('settings loaded ...')
 
-    def _load_project_module(self, project_path):
-        settingsFolder = os.path.join(project_path, self.path_to_settings)
+    def _load_project_module(self, settingsFolder):
         sys.path.insert(0, settingsFolder)
         try:
             settings_mod = __import__(self.settings_module_name)
